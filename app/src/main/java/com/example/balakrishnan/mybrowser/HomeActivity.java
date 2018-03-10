08852c2a029ab7e9a8bcbcaa0ee0760436087140
shrikanth7698 <shrikanth7698@gmail.com>
2018-03-10 20:52:16 +0530

    fixed some bugs

--- app/src/main/java/com/example/balakrishnan/mybrowser/HomeActivity.java
+++ app/src/main/java/com/example/balakrishnan/mybrowser/HomeActivity.java
@@ -102,6 +102,7 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         getSupportActionBar().hide();
 
+
         DisplayMetrics displayMetrics = new DisplayMetrics();
         WindowManager wm = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE); // the results will be higher than using the activity context object or the getWindowManager() shortcut
         wm.getDefaultDisplay().getMetrics(displayMetrics);
@@ -476,6 +477,7 @@ public class HomeActivity extends AppCompatActivity {
 
     }*/
     public void init(){
+        isStoragePermissionGranted();
         progressBar =  findViewById(R.id.progress_bar);
         backgroundIV = findViewById(R.id.backgroundIV);
         backgroundIV.setDrawingCacheEnabled(true);
